951304e7e215a08f6dd029053039af532d1736f8
Rob Hitt <robhitt@gmail.com>
2017-01-20 11:42:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
 'use strict';
+
+function Dog(name, breed, age) {
+  this.name = name
+  this.age = age
+  this.breed = breed
+}
+
+// class Dog {
+//   constructor(name, breed, age) {
+//     this.name = name
+//     this.breed = breed
+//     this.age = age
+//   }
+// }
+
+// dog = new Dog("Pluto", "Dalmation", 21)
+
+function Cat(name, breed, age) {
+  this.name = name
+  this.age = age
+  this.breed = breed
+}
+
+// class Cat {
+//   constructor (name, breed, age) {
+//     this.name = name
+//     this.breed = breed
+//     this.age = age
+//   }
+// }
+
+
+function Ferret(name, age, foodRequirements) {
+  this.name = name
+  this.age = age
+  this.foodRequirements = foodRequirements
+}
+
+// class Ferret {
+//   constructor (name, age, foodRequirements) {
+//     this.name = name
+//     this.foodRequirements = foodRequirements
+//     this.age = age
+//
+//   }
+// }
+
+function Bird(name, breed) {
+  this.name = name
+  this.breed = breed
+}
+
+// class Bird {
+//   constructor (name, breed) {
+//     this.name = name
+//     this.breed = breed
+//   }
+// }
+
+function Fish(name, species, waterConditions) {
+  this.name = name
+  this.species = species
+  this.waterConditions = waterConditions
+}
